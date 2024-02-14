a64bfd166b835cb34899dd8fef603e927ec14c81
Josue Tapia <josue.tapia@live.com.ar>
2024-02-14 06:36:22 -0300

    :sparkles: [ADD] footer responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export function Footer (): JSX.Element {
   return (
-    <footer className="self-end  my-10 ml-10  ">
+    <footer className="self-end py-2 pl-4 lg:my-10 lg:ml-10  ">
       <a href="http://josu.dev" target="_blank" rel="noopener noreferrer"
       className="text-textColor/80 underline underline-offset-1 hover:text-white/80">
       © 2024 JosttMe
