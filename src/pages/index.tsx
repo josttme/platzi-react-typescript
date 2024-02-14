7d1569acc8e8376cb40a3c02731ec54c9cbeb308
Josue Tapia <josue.tapia@live.com.ar>
2024-02-14 06:22:13 -0300

    :art: [UPDATE] button add new cat respnsive gallery

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,10 +19,15 @@ export default function Home (): JSX.Element {
     setViewImage({ url: '' })
   }
   return (
-    <main>
-      <h1 className="font-semibold text-4xl">Hello Josttme</h1>
-      <button onClick={addNewCat}>Add new cat</button>
-
+    <main className='px-1 pb-52 lg:px-4'>
+      <div className='w-full grid gap-5 py-10 place-content-center'>
+      <h1 className="font-semibold text-4xl text-center">Cats Random</h1>
+        <button
+          className="bg-[#0071cf] text-center lg:w-52 lg:h-14 w-36 h-10 fixed bottom-10 left-0 right-0 mx-auto text-lg lg:text-xl grid place-content-center px-4 py-2 rounded-md border border-white/50" type="button"
+          onClick={addNewCat}>
+            Add new cat
+        </button>
+      </div>
       <ResponsiveMasonryGallery
         images={images}
         viewLargeImage={viewLargeImage}
